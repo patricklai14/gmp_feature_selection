bbdececc08ed791046236bda3059a4ae17d34b4a
Patrick Lai <patricklai14@gmail.com>
2021-05-18 13:50:42 -0500

    WIP GA implementation, script for running GA

--- gmp_feature_selection/genetic_algorithm.py
+++ gmp_feature_selection/genetic_algorithm.py
@@ -13,7 +13,7 @@ class genetic_algorithm(gmp_fs.gmp_feature_selector):
     def __init__(self, data, model_eval_params):
         super().__init__(data, model_eval_params)
 
-    def generate_initial_population(self, target_num_groups):
+    def generate_initial_population(self, population_size, target_num_groups):
         population = []
         for i in range(population_size):
             curr_indices = np.random.choice(self.seq_len, target_num_groups, replace=False)
@@ -52,11 +52,35 @@ class genetic_algorithm(gmp_fs.gmp_feature_selector):
 
         return eval_params
 
-    def run(self, population_size, num_generations, target_groups_pct, max_order, enable_parallel=True, 
-            parallel_workspace=None, time_limit="00:30:00", mem_limit=2, conda_env="amptorch", seed=1):
+    #compute fitness scores from MAEs
+    def get_fitness_scores(self, errors):
+        errors_inverted = 1. / np.array(errors)
+
+        #normalize
+        min_inverted_error = np.min(errors_inverted)
+        inverted_error_range = np.max(errors_inverted) - min_inverted_error
+        fitness_scores = (errors_inverted - min_inverted_error) / inverted_error_range
+
+        return fitness_scores
+
+    #reverse sequence mutation
+    def mutate(self, sequence):
+        bounds = np.random.randint(0, len(sequence), size=2)
+        i = bounds.min()
+        j = bounds.max()
+
+        while i < j:
+            tmp = sequence[i]
+            sequence[i] = sequence[j]
+            sequence[j] = tmp
+
+    def run(self, population_size, num_generations, target_groups_pct, max_order, crossover_prob=0.75, 
+            mutation_prob=0.2, elitist=True, enable_parallel=True, parallel_workspace=None, time_limit="00:30:00", 
+            mem_limit=2, conda_env="amptorch", seed=1):
         print("Running genetic algorithm with population_size={}, num_generations={}, target_groups_pct={}, "
-              "max_order={}".format(
-                population_size, num_generations, target_groups_pct, max_order))
+              "max_order={}, crossover_prob={}, mutation_prob={}, elitist={}".format(
+                population_size, num_generations, target_groups_pct, max_order, crossover_prob, mutation_prob, 
+                elitist))
 
         np.random.seed(seed)
         self.model_eval_params["amptorch_config"]["cmd"]["seed"] = seed
@@ -75,17 +99,83 @@ class genetic_algorithm(gmp_fs.gmp_feature_selector):
 
         #generate initial population
         target_num_groups = int(target_groups_pct * len(group_pairs))
-        population = self.generate_initial_population(target_num_groups)
+        population = self.generate_initial_population(population_size, target_num_groups)
 
         #compute fitness of initial population
         eval_params = self.get_model_eval_params_from_genes(population)
-        scores = model_eval.model_evaluation.evaluate_models(
+        errors = model_eval.model_evaluation.evaluate_models(
                     dataset=self.data, config_dicts=eval_params, enable_parallel=enable_parallel,
                     workspace=parallel_workspace, time_limit=time_limit, mem_limit=mem_limit, conda_env=conda_env)
+        fitness_scores = self.get_fitness_scores(errors)
+        print("Fitness scores for initial population: {}".format(fitness))
 
-        for i in range(num_generations):
-            #perform crossover
+        #determine number of 
+        if population_size % 2 == 0 or elitist:
+            num_parent_pairs = population_size / 2
+        else:
+            num_parent_pairs = (population_size / 2) + 1
 
-            #apply mutation
+        for generation in range(num_generations):
+            print("Running genetic algorithm generation {}".format(generation + 1))
+            new_population = []
+
+            #perform elitist selection
+            if elitist:
+                best_seq_idx = np.argmax(fitness_scores)
+                new_population.append(population[best_seq_idx])
+
+            #calculate selection probabilities and perform selection (for roulette wheel selection)
+            print("Selecting parents for crossover")
+            total_score = np.sum(fitness_scores)
+            selection_probs = fitness_scores / total_score
+            parents = zip(np.random.choice(population_size, size=num_parent_pairs, replace=True, p=selection_probs), 
+                          np.random.choice(population_size, size=num_parent_pairs, replace=True, p=selection_probs))
+            print("parents: {}".format(parents))
+
+            for i in range(num_parent_pairs):
+                offspring_1 = []
+                offspring_2 = []
+
+                #determine whether to perform crossover/mutation
+                do_crossover = np.random.rand() < crossover_prob
+                do_mutation_1 = np.random.rand() < mutation_prob
+                do_mutation_2 = np.random.rand() < mutation_prob
+
+                if do_crossover:
+                    #perform crossover (uniform)
+                    gene_origin = np.random.choice([0, 1], size=self.seq_len, replace=True)
+                    for j in range(len(gene_origin)):
+                        offspring.append(parents[i][gene_origin[j]])
+                else:
+                    offspring_1 = parents[0]
+                    offspring_2 = parents[1]
+
+                #apply mutation(s) (reverse sequence)
+                if do_mutation_1:
+                    self.mutate(offspring_1)
+                if do_mutation_2:
+                    self.mutate(offspring_2)
+
+                new_population.append(offspring_1)
+                new_population.append(offspring_2)
+
+            #remove excess sequences. This could be necessary due to elitist selection/odd population size
+            if len(new_population) > population_size:
+                new_population = new_population[:population_size]
 
             #replace old population with new
+            population = new_population
+
+            #evaluate population
+            eval_params = self.get_model_eval_params_from_genes(population)
+            errors = model_eval.model_evaluation.evaluate_models(
+                        dataset=self.data, config_dicts=eval_params, enable_parallel=enable_parallel,
+                        workspace=parallel_workspace, time_limit=time_limit, mem_limit=mem_limit, conda_env=conda_env)
+            fitness_scores = self.get_fitness_scores(errors)
+            print("Fitness scores for generation {}: {}".format(generation + 1, fitness))
+
+        #set best parameters
+        best_idx = np.argmax(fitness_scores)
+        print("Best params: {}".format(eval_params[best_idx]))
+        self.best_error = errors[best_idx]
+        self.best_params = eval_params[best_idx]
---
+++ scripts/aspirin_ga.py
@@ -0,0 +1,134 @@
+import numpy as np
+import pandas as pd
+
+import argparse
+import copy
+import pathlib
+import pdb
+import pickle
+
+from model_eval import model_evaluation
+import gmp_feature_selection as gfs
+
+def main():
+    parser = argparse.ArgumentParser(description="Run genetic algorithm on MD17 dataset")
+    parser.add_argument("--seed", type=int, required=True,
+                        help="random seed to use (in [1, 5])")
+
+    args = parser.parse_args()
+    args_dict = vars(args)
+
+    np.random.seed(args_dict["seed"])
+    run_name = "ga_run_{}".format(args_dict["seed"])
+    top_dir = pathlib.Path("/storage/home/hcoda1/7/plai30")
+
+    #setup dataset (use one of the training sets as validation during feature selection)
+    train_data_file = top_dir / "p-amedford6-0/data/aspirin/aspirin_train_data_{}.p".format(args_dict["seed"])
+    val_data_file_num = 1 if args_dict["seed"] == 5 else args_dict["seed"] + 1 
+    val_data_file = top_dir / "p-amedford6-0/data/aspirin/aspirin_train_data_{}.p".format(val_data_file_num)
+    train_imgs = pickle.load(open(train_data_file, "rb"))
+    val_imgs = pickle.load(open(val_data_file, "rb"))
+ 
+    data = model_evaluation.dataset(train_images=train_imgs,
+                                    test_images=val_imgs[:10000])
+    elements = ["C","H","O"]
+
+    sigmas = [0.25, 1.0, 2.0]
+    gaussians_dir = top_dir / "config/valence_gaussians"
+    gmp_params = {
+        "atom_gaussians": {
+            "C": str(gaussians_dir / "C_pseudodensity_4.g"),
+            "H": str(gaussians_dir / "H_pseudodensity_2.g"),
+            "O": str(gaussians_dir / "O_pseudodensity_4.g")
+        },
+        "cutoff": 10
+    }
+
+    amptorch_config = {
+        "model": {
+            "name":"singlenn",
+            "get_forces": False, 
+            "num_layers": 3, 
+            "num_nodes": 50, 
+            "batchnorm": True
+        },
+        "optim": {
+            "gpus": 0,
+            "force_coefficient": 0.0,
+            "lr": 1e-3,
+            "batch_size": 256,
+            "epochs": 3500,
+            "loss": "mae",
+        },
+        "dataset": {
+            "val_split": 0.2,
+            "elements": elements,
+            "fp_scheme": "mcsh",
+            "fp_params": gmp_params,
+            "save_fps": False,
+            "scaling": {
+                "type": "normalize", 
+                "range": (0, 1),
+                "elementwise":False
+            }
+        },
+        "cmd": {
+            "debug": False,
+            "identifier": "test",
+            "verbose": True,
+            "logger": False
+        }
+    }
+
+    base_config = {
+        "name": run_name,
+        "evaluation_type": "train_test_split",
+        "seed": args_dict["seed"],
+        "amptorch_config": amptorch_config
+    }
+
+    curr_dir = pathlib.Path(__file__).parent.absolute()
+    workspace = curr_dir / "workspace_search_{}".format(run_name)
+
+    ga = gfs.genetic_algorithm(data, base_config)
+    ga.run(population_size=4, num_generations=2, target_groups_pct=0.8, max_order=7, crossover_prob=0.75, 
+           mutation_prob=0.2, enable_parallel=True, parallel_workspace=workspace, time_limit="03:00:00", 
+           mem_limit=2, conda_env="amptorch")
+
+    #print results
+    print("min error: {}, config:{}".format(ga.get_best_error(), ga.get_best_params()))
+
+    #compute test error for best params chosen by the genetic algorithm
+    print("Evaluating best model")
+    num_eval_trials = 5
+    test_configs = []
+    test_datasets = []
+    for i in range(num_eval_trials):
+        curr_test_config = copy.deepcopy(ga.get_best_params())
+        curr_test_config["name"] = "eval_test_{}".format(i + 1)
+        curr_test_config["seed"] = 1
+        curr_test_config["amptorch_config"]["cmd"]["seed"] = 1
+
+        test_configs.append(curr_test_config)
+
+        curr_train_data_file = str(top_dir / "data/aspirin/aspirin_train_data_{}.p".format(i + 1))
+        curr_test_data_file = str(top_dir / "data/aspirin/aspirin_test_data_{}.p".format(i + 1))
+        curr_dataset = model_evaluation.dataset(train_data_files=[curr_train_data_file], test_data_files=[curr_test_data_file])
+        test_datasets.append(curr_dataset)
+
+    curr_dir = pathlib.Path(__file__).parent.absolute()
+    workspace = curr_dir / "workspace_eval_{}".format(run_name)
+    results = model_evaluation.evaluate_models(datasets=test_datasets, config_dicts=test_configs,
+                                               enable_parallel=True, workspace=workspace,
+                                               time_limit="05:00:00", mem_limit=2, conda_env="amptorch")
+
+    #print results
+    errors = [metrics.test_error for metrics in results]
+    print("Test errors: {}".format(errors))
+    print("MAE: {}".format(np.mean(errors)))
+
+
+
+if __name__ == "__main__":
+    main()
+
